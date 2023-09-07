3b0a6ff28f2fc89a81e385ddbab5ed9338f50fc5
John Elliffe <John.Elliffe@dcceew.gov.au>
2023-09-07 13:44:07 +1000

    End of Part 1

--- repositories/product-file.js
+++ repositories/product-file.js
@@ -68,3 +68,87 @@ repo.search = function (search, resolve, reject) {
     });
   }
 };
+
+// Insert a new product object
+repo.insert = function (newData, resolve, reject) {
+  fs.readFile(DATA_FILE, function (err, data) {
+    if (err) {
+      // ERROR: Invoke reject() callback
+      reject(err);
+    } else {
+      // SUCCESS: convert data to JSON
+      let products = JSON.parse(data);
+      // Add new product to array
+      products.push(newData);
+      // Stringify the product array
+      // Save array to the file
+      fs.writeFile(DATA_FILE, JSON.stringify(products), function (err) {
+        if (err) {
+          // ERROR: Invoke reject() callback
+          reject(err);
+        } else {
+          // SUCCESS: Invoke resolve() callback
+          resolve(newData);
+        }
+      });
+    }
+  });
+};
+
+// Update an existing product object
+repo.update = function (changedData, id, resolve, reject) {
+  fs.readFile(DATA_FILE, function (err, data) {
+    if (err) {
+      // ERROR: Invoke reject() callback
+      reject(err);
+    } else {
+      // SUCCESS: Convert to JSON
+      let products = JSON.parse(data);
+      // Find the product to update
+      let product = products.find((row) => row.productID == id);
+      if (product) {
+        // Move changed data into corresponding properties of the existing object
+        Object.assign(product, changedData);
+        // Stringify the product array
+        // Save array to the file
+        fs.writeFile(DATA_FILE, JSON.stringify(products), function (err) {
+          if (err) {
+            // ERROR: Invoke reject() callback
+            reject(err);
+          } else {
+            // SUCCESS: Invoke resolve() callback
+            resolve(product);
+          }
+        });
+      }
+    }
+  });
+};
+
+// Delete an existing product object
+repo.delete = function (id, resolve, reject) {
+  fs.readFile(DATA_FILE, function (err, data) {
+    if (err) {
+      // ERROR: Invoke reject() callback
+      reject(err);
+    } else {
+      // SUCCESS: Convert data to JSON
+      let products = JSON.parse(data);
+      // Find product to delete
+      let index = products.findIndex((row) => row.productID == id);
+      if (index != -1) {
+        // Remove row from array
+        products.splice(index, 1);
+        fs.writeFile(DATA_FILE, JSON.stringify(products), function (err) {
+          if (err) {
+            // ERROR: Invoke reject() callback
+            reject(err);
+          } else {
+            // SUCCESS: Invoke resolve() callback
+            resolve(index);
+          }
+        });
+      }
+    }
+  });
+};
--- routes/product.js
+++ routes/product.js
@@ -115,4 +115,100 @@ router.get("/:id", (req, res, next) => {
   );
 });
 
+// POST Route
+router.post("/", function (req, res, next) {
+  // Pass in the Body from request
+  repo.insert(
+    req.body,
+    function (data) {
+      // SUCCESS: Return status of 201 Created
+      res.status(201).send({
+        status: 201,
+        statusText: "Created",
+        message: "New Product Added.",
+        data: data,
+      });
+    },
+    function (err) {
+      // ERROR: pass error along to
+      // the 'next' middleware
+      next(err);
+    }
+  );
+});
+
+// PUT Route
+router.put("/:id", function (req, res, next) {
+  // Does product to update exist?
+  repo.getById(
+    req.params.id,
+    function (data) {
+      // SUCCESS: Product is found
+      if (data) {
+        // Pass in Body from request
+        repo.update(req.body, req.params.id, function (data) {
+          // SUCCESS: Return 200 OK
+          res.send({
+            status: 200,
+            statusText: "OK",
+            message: `Product '${req.params.id}' updated.`,
+            data: data,
+          });
+        });
+      } else {
+        // Product not found
+        let msg = `The product '${req.params.id}' could not be found.`;
+        res.status(404).send({
+          status: 404,
+          statusText: "Not Found",
+          message: msg,
+          error: {
+            code: "NOT_FOUND",
+            message: msg,
+          },
+        });
+      }
+    },
+    function (err) {
+      // ERROR: pass error along to the 'next' middleware
+      next(err);
+    }
+  );
+});
+
+// DELETE Route
+router.delete("/:id", function (req, res, next) {
+  // Does product to delete exist?
+  repo.getById(
+    req.params.id,
+    function (data) {
+      // SUCCESS: Product is found
+      if (data) {
+        // Pass in 'id' from request
+        repo.delete(req.params.id, function (data) {
+          // SUCCESS: Return 204 No Content
+          res.status(204).send();
+        });
+      } else {
+        // Product not found
+        let msg = `The product '${req.params.id}' could not be found.`;
+        res.status(404).send({
+          status: 404,
+          statusText: "Not Found",
+          message: msg,
+          error: {
+            code: "NOT_FOUND",
+            message: msg,
+          },
+        });
+      }
+    },
+    function (err) {
+      // ERROR: pass error along to
+      // the 'next' middleware
+      next(err);
+    }
+  );
+});
+
 module.exports = router;
